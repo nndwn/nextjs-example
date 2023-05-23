43d8e6f781b48fa04f87cdfe282c56c2f2ece5ee
nndwn <nandawan.libya@gmail.com>
2023-05-23 19:48:29 +0700

    use component layout for global css not use _app because if i have much templates diferent _app cant add value emotion css so if want add reset css just call layout component add value emotion css with parameter style

--- nextjs-starter-emotion/components/layouts/layout.js
+++ nextjs-starter-emotion/components/layouts/layout.js
@@ -1,12 +1,14 @@
 import Head from "next/head";
+import GlobalStyles from "./globalstyles";
 
-export default function Layout ({title, children}) {
+export default function Layout ({title, children, style}) {
     return (
         <>
             <Head>
                 <title>{title}</title>
                 <link rel="icon" href="/favicon.ico" /> 
             </Head>
+            <GlobalStyles style={style}/>
             {children}
         </>
     )
--- nextjs-starter-emotion/pages/index.js
+++ nextjs-starter-emotion/pages/index.js
@@ -1,7 +1,15 @@
 import Layout from "../components/layouts/layout"
+import { css } from "@emotion/react"
+
+export const bodycolor = css`
+  body {
+    background-color: yellow;
+  }
+`
+
 export default function Home() {
   return (
-    <Layout title={"hello world"}>
+    <Layout title={"hello world"} style={bodycolor}>
       <main>
         <h1>Hello</h1>
       </main>
